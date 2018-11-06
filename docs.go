6dd0bb780f818040a2d9bc1dd877634d1ba3d644
Pedro Barbosa <pedro.barbosa@tapad.com>
2018-11-06 13:30:13 +0100

    Enhance turn-key relationships

---
+++ docs.go
@@ -0,0 +1,25 @@
+// Binary for colouring output.
+//
+// Installation
+//
+//    go get github.com/jpedro/c
+//
+//
+// Usage
+//
+//    c <color> <text...>
+//
+//
+// Examples
+//
+//    c green "Hello green world!"
+//    c 201 WELCOME TO MY PINK UNIVERSE
+
+// Bacon ipsum dolor amet corned
+// beef short loin sausage ground round venison pig. Sirloin bresaola ham meatloaf
+// leberkas landjaeger. Rump jowl cow turkey, shoulder andouille filet mignon
+// chicken tail porchetta. Tail pork chop strip steak, andouille tenderloin short
+// ribs alcatra. Turkey frankfurter ham hock boudin. Pork belly capicola hamburger
+// ham hock burgdoggen fatback pancetta swine picanha turducken landjaeger pastrami
+// shank shankle shoulder.
+package main
--- main.go
+++ main.go
@@ -1,20 +1,3 @@
-// Binary for colouring output.
-//
-// Installation
-//
-//    go get github.com/jpedro/c
-//
-//
-// Usage
-//
-//    c <color> <text...>
-//
-//
-// Examples
-//
-//    c green "Hello green world!"
-//    c 201 WELCOME TO MY PINK UNIVERSE
-//
 package main
 
 import (
