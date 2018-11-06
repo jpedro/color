fb5ebb1ce491828a65d1c00040385d4f72a4a6d4
Pedro Barbosa <pedro.barbosa@tapad.com>
2018-11-06 13:27:45 +0100

    Enhance dot-com convergence

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 // Binary for colouring output.
 //
 // Installation
+//
 //    go get github.com/jpedro/c
 //
+//
 // Usage
+//
 //    c <color> <text...>
 //
+//
 // Examples
+//
 //    c green "Hello green world!"
 //    c 201 WELCOME TO MY PINK UNIVERSE
+//
 package main
 
 import (
