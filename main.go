68348263009242a132b36768cf5e99ce0a3f42e2
Pedro Barbosa <pedro.barbosa@tapad.com>
2018-11-06 13:06:11 +0100

    Productize plug-and-play functionalities

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+var colors = map[string]string{
+	"red":    "31;1",
+	"green":  "32;1",
+	"yellow": "33;1",
+	"blue":   "38;5;27;1",
+}
+var colorCode string = "38;5;%s;1"
+
+func main() {
+	color := os.Args[1]
+	code := colors[color]
+
+	if code == "" {
+		code = fmt.Sprintf(colorCode, color)
+	}
+
+	fmt.Printf("\x1b[%sm%s\x1b[0m\n", code, strings.Join(os.Args[2:], " "))
+}
