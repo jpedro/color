3307e334c696821f8e3f63e3f543e7ff8cd74693
Pedro Barbosa <pedro.barbosa@tapad.com>
2018-11-06 13:35:57 +0100

    Target granular convergence

---
+++ doc.go
@@ -0,0 +1,23 @@
+/*
+Binary for colouring output.
+
+
+Installation
+
+To install `c` just run:
+   go get github.com/jpedro/c
+
+
+Usage
+
+The first argument is the color, the rest is the text:
+   c <color> <text...>
+
+
+Examples
+
+Some eaamples:
+   c green "Hello green world!"
+   c 201 WELCOME TO MY PINK UNIVERSE
+*/
+package main
