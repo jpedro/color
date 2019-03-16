09601fdfb38bb6dc3962b0c2934485193d960cae
Pedro Barbosa <pedro.barbosa@tapad.com>
2019-03-16 13:40:27 +0100

    Expedite real-time deliverables

--- c/doc.go
+++ c/doc.go
@@ -13,16 +13,17 @@ Binary for colouring output.
 
 Installation
 
-To install ``c`` just run:
+To install the ``c`` binary run:
 
-   go get github.com/jpedro/color/c
+		go get github.com/jpedro/color/c
 
 
 Usage
 
 The first argument is the color, the rest is the text. If the color is an integer
 it will be passed to as the __38;5;x__ code.
-   c <color> <text...>
+
+		c <color> <text...>
 
 
 Examples
@@ -30,8 +31,8 @@ Examples
 A header needs a text after. Otherwise the code below renders it a normal paragraph.
 But here are the examples:
 
-   c green "Hello green world!"
-   c 201 WELCOME TO MY PINK UNIVERSE
+		c green "Hello green world!"
+   	c 201 WELCOME TO MY PINK UNIVERSE
 
 */
 package main
