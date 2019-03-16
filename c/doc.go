41e46b304652128fe1620079b054a01258d84d4e
Pedro Barbosa <pedro.barbosa@tapad.com>
2019-03-16 13:40:50 +0100

    When the green woods laugh

--- c/doc.go
+++ c/doc.go
@@ -15,7 +15,7 @@ Installation
 
 To install the ``c`` binary run:
 
-		go get github.com/jpedro/color/c
+    go get github.com/jpedro/color/c
 
 
 Usage
@@ -23,7 +23,7 @@ Usage
 The first argument is the color, the rest is the text. If the color is an integer
 it will be passed to as the __38;5;x__ code.
 
-		c <color> <text...>
+    c <color> <text...>
 
 
 Examples
@@ -31,8 +31,8 @@ Examples
 A header needs a text after. Otherwise the code below renders it a normal paragraph.
 But here are the examples:
 
-		c green "Hello green world!"
-   	c 201 WELCOME TO MY PINK UNIVERSE
+    c green "Hello green world!"
+    c 201 WELCOME TO MY PINK UNIVERSE
 
 */
 package main
