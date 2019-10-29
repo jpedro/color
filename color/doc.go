920193c04ea466f973b883edfe8cbbc28b862e48
Pedro Barbosa <jpedro.barbosa@gmail.com>
2019-10-29 06:24:12 +0100

    why is everything broken

--- color/doc.go
+++ color/doc.go
@@ -1,11 +1,13 @@
 // Bacon ipsum dolor amet corned
 //
-// Beef short loin sausage ground round venison pig. Sirloin bresaola ham meatloaf
-// leberkas landjaeger. Rump jowl cow turkey, shoulder andouille filet mignon
-// chicken tail porchetta. Tail pork chop strip steak, andouille tenderloin short
-// ribs alcatra. Turkey frankfurter ham hock boudin. Pork belly capicola hamburger
-// ham hock burgdoggen fatback pancetta swine picanha turducken landjaeger pastrami
-// shank shankle shoulder.
+// This text shall ne hereby ignored by https://godoc.org/
+//
+// Beef short loin sausage ground round venison pig. Sirloin bresaola ham
+// meatloaf leberkas landjaeger. Rump jowl cow turkey, shoulder andouille filet
+// mignon chicken tail porchetta. Tail pork chop strip steak, andouille
+// tenderloin short ribs alcatra. Turkey frankfurter ham hock boudin. Pork belly
+// capicola hamburger ham hock burgdoggen fatback pancetta swine picanha
+// turducken landjaeger pastrami shank shankle shoulder.
 
 /*
 Binary for colouring output.
@@ -13,9 +15,9 @@ Binary for colouring output.
 
 Installation
 
-To install the ``c`` binary run:
+To install the ``color`` binary run:
 
-    go get github.com/jpedro/color/c
+    go get github.com/jpedro/color/color
 
 
 Usage
@@ -23,16 +25,24 @@ Usage
 The first argument is the color, the rest is the text. If the color is an integer
 it will be passed to as the __38;5;x__ code.
 
-    c <color> <text...>
+    color <color> <text...>
 
+You can also pass a named color. The list is:
+
+    - red
+    - green
+    - yellow
+    - blue
+    - magenta
+    - cyan
 
 Examples
 
 A header needs a text after. Otherwise the code below renders it a normal paragraph.
 But here are the examples:
 
-    c green "Hello green world!"
-    c 201 WELCOME TO MY PINK UNIVERSE
+    color 208 WELCOME TO MY ORANGE UNIVERSE
+    color green "Hello green world!"
 
 */
 package main
